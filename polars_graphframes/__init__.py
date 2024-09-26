dd3c718e80f332a2649caab05c9fe640f6a476a6
Tomáš Děd <tomas-ded@seznam.cz>
2024-09-26 23:50:19 +0200

    fix mypy issue with str

--- polars_graphframes/__init__.py
+++ polars_graphframes/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import TYPE_CHECKING, Iterable
+from typing import TYPE_CHECKING, Iterable, cast
 
 import polars as pl
 from polars.plugins import register_plugin_function
@@ -19,10 +19,10 @@ def get_cluster_ids(node_definition: IntoExpr, edge_definitions: Iterable[IntoEx
 
     if isinstance(node_definition, str):
         node_definition = F.col(node_definition)
-    edge_definitions = [edge_definition if isinstance(edge_definition, pl.Expr) else F.col(edge_definition) for edge_definition in edge_definitions]
+    edge_definitions_exprs: list[pl.Expr | pl.Series] = [F.col(edge_definition) if isinstance(edge_definition, str) else edge_definition for edge_definition in edge_definitions]
 
     return register_plugin_function(
-        args=[node_definition.hash(seed=0)] + [edge_definition.hash(1 + i) for i, edge_definition in enumerate(edge_definitions)],
+        args=[node_definition.hash(seed=0)] + [edge_definition.hash(1 + i) for i, edge_definition in enumerate(edge_definitions_exprs)],
         plugin_path=LIB,
         function_name="get_cluster_ids",
         is_elementwise=False,
