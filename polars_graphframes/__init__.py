1ffe2bd35eb3b041dcc88785a9ca7397b6ada8e6
Tomáš Děd <tomas-ded@seznam.cz>
2024-09-26 23:52:35 +0200

    aaand ruff issue

--- polars_graphframes/__init__.py
+++ polars_graphframes/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import TYPE_CHECKING, Iterable, cast
+from typing import TYPE_CHECKING, Iterable
 
 import polars as pl
 from polars.plugins import register_plugin_function
